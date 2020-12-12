9681562f54ec528a615822e1ba5b0e62239c5916
reaper47 <macpoule@gmail.com>
2020-12-12 14:02:32 +0100

    added instruments in a box

--- lib/providers/repository.dart
+++ lib/providers/repository.dart
@@ -2,7 +2,48 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive/hive.dart';
 
 import 'package:musicavis/repository/boxes.dart';
+import 'package:musicavis/utils/extensions.dart';
 
-final instrumentsProvider = Provider<Map<dynamic, String>>((ref) {
-  return Hive.box<String>(INSTRUMENTS_BOX).toMap();
-});
+class InstrumentItem {
+  int id;
+  String name;
+  bool isSelected;
+
+  InstrumentItem({this.id, String name, this.isSelected = false}) {
+    this.name = name.capitalize();
+  }
+}
+
+class InstrumentList extends StateNotifier<List<InstrumentItem>> {
+  List<String> _selectedItems = [];
+  final _box = Hive.box<String>(INSTRUMENTS_BOX);
+
+  InstrumentList(List<InstrumentItem> items) : super(items);
+
+  void add(String name) {
+    if (_box.get(name.toLowerCase()) == null) {
+      _box.add(name.toLowerCase());
+      state = [
+        ...state,
+        InstrumentItem(
+          id: state.length + 1,
+          name: name.capitalize(),
+        )
+      ];
+    }
+  }
+
+  void toggle(int id) {
+    state = [
+      for (final item in state)
+        if (item.id == id)
+          InstrumentItem(
+            id: item.id,
+            name: item.name,
+            isSelected: !item.isSelected,
+          )
+        else
+          item,
+    ];
+  }
+}
---
+++ lib/utils/extensions.dart
@@ -0,0 +1,4 @@
+extension StringExtension on String {
+  String capitalize() =>
+      '${this[0].toUpperCase()}${this.substring(1).toLowerCase()}';
+}
